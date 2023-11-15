06d6dbe87550d1780325337455c5eaa22e2e77da
Nicolas Delille <delille.n@gmail.com>
2023-11-15 15:20:04 +0100

    ok

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,20 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+resto1 = {name: "Saigon", address: "5 rue du Temple 75013 Paris", phone_number: "0134267489", category: "chinese"}
+resto2 = {name: "Pazza Pizzas", address: "18 avenue du Général Leclerc 35000 Rennes", phone_number: "0243448909", category: "italian"}
+resto3 = {name: "Itagaki", address: "37 avenue de Rivoli 44000 Nantes", phone_number: "0267859365", category: "japanese"}
+resto4 = {name: "Choucroute Party", address: "46 rue fontaine 44390 Nort sur Erdre", phone_number: "0254789138", category: "french"}
+resto5 = {name: "A la bonne frite", address: "4 avenue de l'obélisque 69000 Lyon", phone_number: "0176389472", category: "belgian"}
+
+
+[resto1, resto2, resto3, resto4, resto5].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
